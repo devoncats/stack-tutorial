35f7790387b8563df4ea41166d88a2145e3f6191
Devoncats <devoncats.dev@outlook.com>
2025-09-14 02:14:24 -0500

    fix: linting errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={cn(
           "bg-background min-h-screen font-sans antialiased",
-          fontSans.variable
+          fontSans.variable,
         )}
       >
         {children}
